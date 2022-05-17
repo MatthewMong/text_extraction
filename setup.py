79c0cbcf5e4502cfc29affa2d8a197dc95ba57c3
Matthew Mong <matthew.mong1999@gmail.com>
2022-05-17 14:39:42 -0700

    add flask as a dependency

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='text_extraction',
+      version='0.1',
+      description='some text processing',
+      url='https://github.com/MatthewMong/text_extraction',
+      author='Matthew Mong',
+      author_email='matthew.mong1999@gmail.com',
+      license='MIT',
+      packages=['flask'],
+      install_requires=[
+          'markdown',
+      ],
+      zip_safe=False)
