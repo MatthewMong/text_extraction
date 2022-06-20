be8806741e0f1be94ce26702572b9bb15e6b7f01
Matthew Mong <matthew.mong1999@gmail.com>
2022-06-20 14:09:35 -0700

    added control groups

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask import request
 from Bio import Entrez
 from spacy.matcher import Matcher
-from spacy import displacy
 import spacy
 
 nlp = spacy.load("en_core_web_trf")
@@ -10,13 +9,43 @@ nlp = spacy.load("en_core_web_trf")
 n_matcher = Matcher(nlp.vocab)
 sex_matcher = Matcher(nlp.vocab)
 age_matcher = Matcher(nlp.vocab)
+bio_fluid_matcher = Matcher(nlp.vocab)
+omic_matcher = Matcher(nlp.vocab)
+control_group_matcher = Matcher(nlp.vocab)
+healthy_control_group_matcher = Matcher(nlp.vocab)
+
+control = [{"LEMMA": "control"}, {"LEMMA": "group"}]
+healthy_control = [{"LEMMA": "healthy"},{"LEMMA": "control"}, {"LEMMA": "group", "OP": "?"}]
+control_group_matcher.add("controlGroup", [control])
+healthy_control_group_matcher.add("healthyControlGroup", [healthy_control])
+
+metabolomics = [{"LEMMA": {"IN": ["metabolomics", "metabolic", "metabolite"]}}]
+proteomics = [{"LEMMA": {"IN": ["proteomic", "protein"]}}]
+genomics = [{"LEMMA": {"IN": ["genomic", "genome", "gene"]}}]
+microbiomics = [{"LEMMA": {"IN": ["microbiomic", "microbiome", "bacterium", "fungus", "virus"]}}]
+multiomics = [{"LEMMA": {"IN": ["multiomic"]}}]
+
+omic_matcher.add('metabolomics', [metabolomics])
+omic_matcher.add('proteomics', [proteomics])
+omic_matcher.add('genomics', [genomics])
+omic_matcher.add('microbiomics', [microbiomics])
+omic_matcher.add('multiomics', [multiomics])
+
+
+bio_fluid_category = [{"LEMMA": {"IN": ["serum", "plasma", "tissue", "urine"]}}]
+red_blood_cells = [{"LOWER": "red"}, {"LOWER": "blood"}, {"LEMMA": "cell"}]
+rbc = [{"LOWER": "rbc"}]
+bio_fluid_matcher.add('category', [bio_fluid_category])
+bio_fluid_matcher.add('red_blood_cells', [red_blood_cells])
+bio_fluid_matcher.add('rbc', [rbc])
+
 
 age_category = [{"LEMMA":{"IN": ["adult", "teen", "child", "infant"]}}]
 age_year = [{"LEMMA":{"IN": ["age, year"]}}]
 age_matcher.add('category', [age_category])
 age_matcher.add('year', [age_year])
-subject_pattern = [{"ENT_TYPE": {"IN" : ["CARDINAL"]}, "OP": "+"}, {"POS": "ADJ", "OP": "*"}, {"POS": "NOUN", "TEXT": {"NOT_IN": ["%"]}, "LENGTH": {">": 2}}]
 
+subject_pattern = [{"ENT_TYPE": {"IN" : ["CARDINAL"]}, "OP": "+"}, {"POS": "ADJ", "OP": "*"}, {"POS": "NOUN", "TEXT": {"NOT_IN": ["%"]}, "LENGTH": {">": 2}}]
 acronym_pattern = [{"LIKE_NUM": True}, {"IS_UPPER": True}]
 n_pattern = [{"TEXT": {"REGEX": "^n="}}]
 n_spaces_pattern = [{"LOWER": "n"}, {"TEXT": "="}, {"LIKE_NUM": True}]
@@ -57,7 +86,7 @@ def entrance():
         pmid = int(str(pubmed_article['MedlineCitation']['PMID']))
         article = pubmed_article['MedlineCitation']['Article']
         if 'Abstract' in article:
-            abstract = ' '.join(article['Abstract']['AbstractText'])
+            abstract = ' '.join(article['Abstract']['AbstractText']).replace(',', '')
             abstract_dict[pmid] = abstract.encode("ascii", "ignore").decode()
         else:
             without_abstract.append(pmid)
@@ -75,14 +104,37 @@ def entrance():
         potential_sexes = []
         for match_id, start, end in sexes:
             potential_sexes.append(text[start:end])
+        if len(potential_sexes) == 0:
+            potential_sexes.append("both")
+        
+        fluids = bio_fluid_matcher(text)
+        potential_fluids = []
+        for match_id, start, end in fluids:
+            potential_fluids.append(text[start:end])
+        
         ages = age_matcher(text)
         potential_ages = []
         for match_id, start, end in ages:
             potential_ages.append(text[start:end])
         if len(potential_ages) == 0:
             potential_ages.append("adult")
+        
+        omics = omic_matcher(text)
+        potential_omics = []
+        for match_id, start, end in omics:
+            potential_omics.append(text[start:end])
+
+        control_groups = control_group_matcher(text)
+        potential_control_groups = []
+        for match_id, start, end in control_groups:
+            potential_control_groups.append(text[start:end])
+        
+        healthy_control_groups = healthy_control_group_matcher(text)
+        potential_healthy_control_groups = []
+        for match_id, start, end in healthy_control_groups:
+            potential_healthy_control_groups.append(text[start:end])
     if (len(abstract_dict.items()) > 0):
-        return f'<b>input</b><br/>{text}<br/><b>possible sample sizes:</b><br/>{potential_n}<br/><b>possible sexes:</b><br/>{set(potential_sexes)}<br/><b>possible ages:</b><br/>{set(potential_ages)}'
+        return f'<b>input</b><br/>{text}<br/><b>possible sample sizes:</b><br/>{potential_n}<br/><b>possible sexes:</b><br/>{set(potential_sexes)}<br/><b>possible ages:</b><br/>{set(potential_ages)}<br/><b>possible fluids:</b><br/>{set(potential_fluids)}<br/><b>possible omics:</b><br/>{set(potential_omics)}<br/><b>possible control group:</b><br/>{len(potential_control_groups) != 0}<br/><b>possible healthy control group:</b><br/>{len(potential_healthy_control_groups) != 0}'
     else:
         return '<p>no abstract</p>'
 
